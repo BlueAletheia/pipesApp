a096834b8575fd91675a2e0ef92807d02d92ecca
Alicia Fernandez <alicia.fernandez@bosonit.com>
2022-08-11 14:52:51 +0200

    pipesApp beginnings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
+import { compileNgModule } from '@angular/compiler';
 import { Component } from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipesApp';
+  nombre: string = 'Alicia Fernández';
+  valor: number = 1000;
+  obj = {
+    nombre: 'Fernando',
+  };
+
+  mostrarNombre() {
+    console.log(this.nombre);
+    console.log(this.valor);
+    console.log(this.obj);
+  }
 }
+     
